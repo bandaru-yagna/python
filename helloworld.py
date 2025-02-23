3557df4dd2d304fa1cd6a09c439cfc5af19d4646
bandaru-yagna <bandaruyagna@gmail.com>
2025-02-23 21:39:28 +0530

    assignment

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+
+username=input("Enter your username:")
+passwod=input("Enter your password:")
+
+db_username="python"
+db_password="@yagna"
+
+
+print(username==db_username and password==db_password)
