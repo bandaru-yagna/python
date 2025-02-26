230f418a3879361021b71ceca883b4d56334b841
bandaru-yagna <bandaruyagna@gmail.com>
2025-02-26 17:30:27 +0530

    Delete helloworld.py

--- helloworld.py
+++
@@ -1,9 +0,0 @@
-
-username=input("Enter your username:")
-passwod=input("Enter your password:")
-
-db_username="python"
-db_password="@yagna"
-
-
-print(username==db_username and password==db_password)
